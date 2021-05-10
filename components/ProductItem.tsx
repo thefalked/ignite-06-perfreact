9ced843721aaa667f07b8340ca91c37bef0325e3
thefalked <giulainocrivelli@hotmail.com>
2021-05-09 23:41:36 -0400

    adding lodash (on pourpuse) to see on next bundle analizer

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,6 +1,8 @@
 import { memo, useState } from "react";
-import { AddProductionToWishListProps } from "./AddProductToWishList";
 import dynamic from "next/dynamic";
+import lodash from "lodash";
+
+import { AddProductionToWishListProps } from "./AddProductToWishList";
 
 // import { AddProductionToWishList } from "./AddProductToWishList";
 const AddProductionToWishList = dynamic<AddProductionToWishListProps>(
@@ -46,6 +48,6 @@ function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
 export const ProductItem = memo(
   ProductItemComponent,
   (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
+    return lodash.isEqual(prevProps.product, nextProps.product);
   }
 );
