9d61ecb9fe54c4b0785285fbf59d682513d05158
thefalked <giulainocrivelli@hotmail.com>
2021-05-09 23:30:46 -0400

    using virtualization for extensive information

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from "react-virtualized";
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -16,10 +18,28 @@ export function SearchResults({
   results,
   onAddToWishList,
 }: SearchResultsProps) {
+  const rowRender: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map((product) => {
+      <List
+        height={300}
+        rowHeight={25}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRender}
+      />
+      {/* {results.map((product) => {
         return (
           <ProductItem
             key={product.id}
@@ -27,7 +47,7 @@ export function SearchResults({
             onAddToWishList={onAddToWishList}
           />
         );
-      })}
+      })} */}
     </div>
   );
 }
